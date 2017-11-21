86cff560ea98fc7c46c2649b88b3b9082d8e9af1
edkeonl <edkeonl@gmail.com>
2017-11-21 16:24:38 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import requests
-import urllib.request, urllib.parse, urllib.error
+import urllib, urllib.request, urllib.parse, urllib.error
 import time
 from datetime import date
 
@@ -23,7 +23,7 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    res = makeWebhookResult(req)
 
     res = json.dumps(res, indent=4)
     print(res)
@@ -31,59 +31,28 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-
-def processRequest(req):
+def makeWebhookResult(req):
     if req.get("result").get("action") != "coin_change":
         return {}
-        
-    baseurl = "https://api.coinmarketcap.com/v1/ticker/"
-    yql_query = makeCoinQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + yql_query + "/"
-    print(yql_url)
-    
-    r = requests.get(yql_url)
+    result = req.get("result")
+    parameters = result.get("parameters")
+    zone = parameters.get("shipping-zone")
 
-    curr_price = requests.get(r.url).json()
-    percent = curr_price[0]['percent_change_24h']
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    print("yql result: ")
-    print(result)
-
-    speech = "hey i am a coin"
-    #speech = coin_type + "changed" + percent + "% within the last 24 hours"
+    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
     print("Response:")
     print(speech)
-    
-    #result = req.get("result")
-    #parameters = result.get("parameters")
-    #zone = parameters.get("shipping-zone")
-
-    #cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
-
-    #speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
     return {
         "speech": speech,
         "displayText": speech,
         #"data": {},
         # "contextOut": [],
-        "source": "api-coinmarketcap"
+        "source": "apiai-onlinestore-shipping"
     }
-    
-    return res
 
-
-def makeCoinQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    coin_type = parameters.get("cryptocurrency")
-    if coin_type is None:
-        return None
-
-    return coin_type
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
