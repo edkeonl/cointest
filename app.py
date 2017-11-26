39fb03c94b94cc0973dabbd6c0bc1861657330d0
edkeonl <edkeonl@gmail.com>
2017-11-26 18:15:09 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+#import requests
 import urllib
+#urllib.request, urllib.parse, urllib.error
+
 import json
 import os
 
@@ -11,6 +14,7 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
@@ -18,22 +22,13 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = makeWebhookResult(req)
-
-    res = json.dumps(res, indent=4)
-    #print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-def makeWebhookResult(req):
     if req.get("result").get("action") != "coin_change":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    coin = parameters.get("cryptocurrency")
+    zone = parameters.get("cryptocurrency")
 
-    speech = "I hope this works" + coin 
+    speech = "I hope this works"
 
     print("Response:")
     print(speech)
@@ -46,9 +41,15 @@ def makeWebhookResult(req):
         "source": "apiai-slack-richformatting"
     }
 
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
     print "Starting app on port %d" % port
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=True, port=port, host='0.0.0.0')
