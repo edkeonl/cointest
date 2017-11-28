e3a8891fffa60f8be42ac9f9f1c80082018cfd9a
edkeonl <edkeonl@gmail.com>
2017-11-29 02:36:36 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
+import requests
 import urllib
 import json
 import os
-import requests
 
 from flask import Flask
 from flask import request
@@ -27,7 +27,7 @@ def webhook():
     
     baseurl = "https://api.coinmarketcap.com/v1/ticker/"
     coin_url = baseurl + coin_type
-    jsonifed_resp = requests.get(coin_url).json()
+    jsonifed_resp = requests.get(str(coin_url)).json()
     
     #coin_name = str(jsonifed_resp[0]['id'])
     #coin_price = str(jsonifed_resp[0]['price_usd'])
