c08caa2cfe3f8dddcacbc64230c3499291504304
edkeonl <edkeonl@gmail.com>
2017-11-29 02:16:12 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -26,19 +26,15 @@ def webhook():
     parameters = result.get("parameters")
     coin_type = parameters.get("cryptocurrency")
     
-    baseurl = "https://api.coinmarketcap.com/v1/ticker/"
-    coin_url = baseurl + coin_type
+    #baseurl = "https://api.coinmarketcap.com/v1/ticker/"
+    #coin_url = baseurl + coin_type
+    #jsonifed_resp = requests.get(coin_url).json()
     
-    #rr = requests.get(coin_url)
-    #Json decoding
-    #bitdata = rr.text
-    #bitjson = json.loads(bitdata)
-    jsonifed_resp = requests.get(coin_url).json()
-    
-    coin_name = str(jsonifed_resp[0]['id'])
-    coin_price = str(jsonifed_resp[0]['price_usd'])
+    #coin_name = str(jsonifed_resp[0]['id'])
+    #coin_price = str(jsonifed_resp[0]['price_usd'])
 
-    speech = coin_name + " is currently " + coin_price + " US dollars"
+    #speech = coin_name + " is currently " + coin_price + " US dollars"
+    speech = "coin coin coin"
     
     print("Response:")
     print(speech)
