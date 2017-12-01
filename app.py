0b48fbd35e6a5a867035f313924a8698446120d8
edkeonl <edkeonl@gmail.com>
2017-12-02 00:39:43 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def makeCoinQuery(req):
     speech = coin_name + " is currently " + coin_price + " US Dollars"
     
     if str(data[0]['symbol']) == 'BTC'
-        temp_r = requests.get('https://api.coinone.co.kr/ticker/?currency=bch')
-        coinone_value = requests.get(temp_r.url).json()
+        temp_r = urllib.request.urlopen('https://api.coinone.co.kr/ticker/?currency=bch').read()
+        coinone_value = json.loads(temp_r)
         coinone_price = coinone_value['last']
         speech = coin_name + " is currently $" + coin_price + ", Coinone price is " + coinone_price + "KRW"
     
